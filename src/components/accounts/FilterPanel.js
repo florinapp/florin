6274df8301bf8895a1655ec9ce6e00af5d6e22db
Kevin Jing Qiu <kevin@idempotent.ca>
2017-04-07 23:55:59 -0400

    Refactoring and style fixes

--- src/components/accounts/FilterPanel.js
+++ src/components/accounts/FilterPanel.js
@@ -1,54 +1,70 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-const FilterPanel = ({currentAccountId, filter}) => {
-    console.log(filter)
+const toBool = (val) => {
+    if (typeof(val) === 'boolean') {
+        return val
+    }
+    if (val.toLowerCase() === 'true') {
+        return true
+    }
+    return false
+}
+
+const q = (filter, replace) => {
+    let params = {
+        ...filter,
+        ...replace
+    }
+    return Object.keys(params).map((k) => `${encodeURIComponent(k)}=${encodeURIComponent(params[k])}`).join('&')
+}
+
+const DateRangeFilterPanel = ({ currentAccountId, filter }) => {
     const dateRangeFilters = [
-        {name: 'thisMonth', caption: 'This Month'},
-        {name: 'lastMonth', caption: 'Last Month'},
-        {name: 'thisYear', caption: 'This Year'}
+        { name: 'thisMonth', caption: 'This Month' },
+        { name: 'lastMonth', caption: 'Last Month' },
+        { name: 'thisYear', caption: 'This Year' }
     ]
 
-    const q = (filter, replace) => {
-        let params = {
-            ...filter,
-            ...replace
-        }
-        return Object.keys(params).map((k) => `${encodeURIComponent(k)}=${encodeURIComponent(params[k])}`).join('&')
-    }
-
-    const toBool = (val) => {
-        if (typeof(val) === 'boolean') {
-            return val
-        }
-        if (val.toLowerCase() === 'true') {
-            return true
-        }
-        return false
-    }
     const { currentDateRange } = filter
+
     return (
-        <div className="filter-panel">
-            <ul className="nav nav-pills">
-                {dateRangeFilters.map((dateRangeFilter) => (
-                        <li key={dateRangeFilter.name} role="presentation"
-                            className={currentDateRange === dateRangeFilter.name ? "active" : ""}>
-                            <NavLink to={`/accounts/${currentAccountId}?${q(filter, {currentDateRange: dateRangeFilter.name})}`}>
-                                {dateRangeFilter.caption}
-                            </NavLink>
-                        </li>
-                ))}
-            </ul>
-            <ul className="nav nav-pills">
-                <li key="onlyUncategorized" role="presentation" className={toBool(filter.onlyUncategorized) ? "active" : ""}>
-                    <NavLink to={`/accounts/${currentAccountId}?${q(filter, {onlyUncategorized: !(toBool(filter.onlyUncategorized))})}`}>
-                        Show Only Uncategorized
+        <ul className="nav nav-pills">
+            {dateRangeFilters.map((dateRangeFilter) => (
+                <li key={dateRangeFilter.name} role="presentation"
+                    className={currentDateRange === dateRangeFilter.name ? "active" : ""}>
+                    <NavLink to={`/accounts/${currentAccountId}?${q(filter, { currentDateRange: dateRangeFilter.name })}`}>
+                        {dateRangeFilter.caption}
                     </NavLink>
                 </li>
-                <li>
-                    <NavLink to="#">Show Excluded</NavLink>
-                </li>
-            </ul>
+            ))}
+        </ul>
+    )
+}
+
+const SpecialFilterPanel = ({ currentAccountId, filter }) => {
+
+    return (
+        <ul className="nav nav-pills">
+            <li role="presentation" className={toBool(filter.onlyUncategorized) ? "active" : ""}>
+                <NavLink to={`/accounts/${currentAccountId}?${q(filter, { onlyUncategorized: !(toBool(filter.onlyUncategorized)) })}`}>
+                    Show Only Uncategorized
+                </NavLink>
+            </li>
+            <li role="presentation" className={toBool(filter.includeExcluded) ? "active" : ""}>
+                <NavLink to={`/accounts/${currentAccountId}?${q(filter, { includeExcluded: !(toBool(filter.includeExcluded)) })}`}>
+                    Show Excluded
+                </NavLink>
+            </li>
+        </ul>
+    )
+}
+
+const FilterPanel = ({currentAccountId, filter}) => {
+    return (
+        <div className="filter-panel">
+            <DateRangeFilterPanel currentAccountId={currentAccountId} filter={filter} />
+            <SpecialFilterPanel currentAccountId={currentAccountId} filter={filter} />
             <hr />
         </div>
     )
