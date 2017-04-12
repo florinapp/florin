26d335714ff1fae122738fc08b9035dc0f3b61a6
Kevin Jing Qiu <kevin@idempotent.ca>
2017-04-12 00:32:05 -0400

    date range test

--- server/tests/integration/test_transactions.py
+++ server/tests/integration/test_transactions.py
@@ -1,8 +1,9 @@
+import datetime
 import pytest
 import requests
 from .utils import reset_database
-from .fixtures.transactions import create
-from .fixtures.accounts import tangerine_credit_card_account
+from .fixtures.transactions import create, fake
+from .fixtures.accounts import tangerine_credit_card_account, rogers_bank_credit_card_account
 
 
 def setup_function(function):
@@ -44,3 +45,43 @@ def test_transactions_get___pagination___edge_case(tangerine_credit_card_account
     response_json = response.json()
     assert response_json['current_page'] == 3
     assert response_json['total_pages'] == 3
+
+
+def test_transactions_get___transactions_from_multiple_accounts_are_available_under_all(
+        tangerine_credit_card_account, rogers_bank_credit_card_account):
+    for _ in xrange(5):
+        create(account=tangerine_credit_card_account['id'])
+    for _ in xrange(5):
+        create(account=rogers_bank_credit_card_account['id'])
+    response = requests.get('http://localhost:7000/api/accounts/_all')
+    assert response.status_code == 200
+    response_json = response.json()
+    assert response_json['total_pages'] == 1
+    assert response_json['current_page'] == 1
+
+
+def test_transactions_get___transactions_in_date_range(tangerine_credit_card_account):
+    # before startDate
+    for _ in xrange(2):
+        create(account=tangerine_credit_card_account['id'],
+               date=fake.date_time_between_dates(
+                   datetime_start=datetime.datetime(2017, 1, 1),
+                   datetime_end=datetime.datetime(2017, 2, 28)).date())
+    # between startDate and endDate
+    for _ in xrange(3):
+        create(account=tangerine_credit_card_account['id'],
+               date=fake.date_time_between_dates(
+                   datetime_start=datetime.datetime(2017, 3, 1),
+                   datetime_end=datetime.datetime(2017, 3, 13)).date())
+    # on endDate
+    create(account=tangerine_credit_card_account['id'], date=datetime.date(2017, 3, 13))
+    for _ in xrange(3):
+        create(account=tangerine_credit_card_account['id'],
+               date=fake.date_time_between_dates(
+                   datetime_start=datetime.datetime(2017, 3, 14),
+                   datetime_end=datetime.datetime(2017, 3, 31)).date())
+    response = requests.get('http://localhost:7000/api/accounts/_all?'
+                            'startDate=2017-03-01&endDate=2017-03-13')
+    assert response.status_code == 200
+    response_json = response.json()
+    assert 4 == len(response_json['transactions'])
