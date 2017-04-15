125bea3c9e9050228885a3660ee99e862e655114
Kevin Jing Qiu <kevin@idempotent.ca>
2017-04-14 21:14:04 -0400

    Refactor into filter and paginator

--- server/florin/services/transactions.py
+++ server/florin/services/transactions.py
@@ -3,44 +3,63 @@ from asbool import asbool
 from .params import get_date_range_params
 from .categories import TBD_CATEGORY_ID, INTERNAL_TRANSFER_CATEGORY_ID
 from . import accounts, exceptions
-from pony.orm import commit, db_session, TransactionIntegrityError, CacheIndexError
+from pony.orm import commit
+
+
+class Paginator(object):
+    def __init__(self, args):
+        self._args = args
+        self.per_page = int(args.get('perPage', '10'))
+        self.page = int(args.get('page', '1'))
+        self.total = None
+
+    def __call__(self, query):
+        total = query.count()
+        self.total_pages = int(math.ceil(1.0 * total / self.per_page))
+        query = query.limit(self.per_page, offset=(self.page - 1) * self.per_page)
+        return query
+
+
+class TransactionFilter(object):
+    def __init__(self, account, args):
+        self._args = args
+        self.start_date, self.end_date = get_date_range_params(args)
+        self.include_internal_transfer = asbool(args.get('includeInternalTransfer', 'false'))
+        self.only_uncategorized = asbool(args.get('onlyUncategorized', 'false'))
+        self.account = account
+
+    def __call__(self, query):
+        query = query.filter(lambda t: t.date >= self.start_date and t.date <= self.end_date)
+
+        if not self.include_internal_transfer:
+            query = query.filter(lambda t: t.category_id != INTERNAL_TRANSFER_CATEGORY_ID)
+
+        if self.only_uncategorized:
+            query = query.filter(lambda t: t.category_id == TBD_CATEGORY_ID)
+
+        if self.account is not accounts.ALL_ACCOUNTS:
+            query = query.filter(lambda t: t.account == self.account.id)
+
+        return query
 
 
 def get(app, account_id, args):
-    start_date, end_date = get_date_range_params(args)
-
-    include_internal_transfer = asbool(args.get('includeInternalTransfer', 'false'))
-
-    only_uncategorized = asbool(args.get('onlyUncategorized', 'false'))
-
-    per_page = int(args.get('perPage', '10'))
-
-    page = int(args.get('page', '1'))
+    account = accounts.get_by_id(app, account_id)
+    filter = TransactionFilter(account, args)
+    paginator = Paginator(args)
 
     Transaction = app.db.Transaction
 
-    query = Transaction.select(
-        lambda t: t.date >= start_date
-        and t.date <= end_date
-    )
+    query = Transaction.select()
+    query = filter(query)
 
-    if not include_internal_transfer:
-        query = query.filter(lambda t: t.category_id != INTERNAL_TRANSFER_CATEGORY_ID)
-
-    if only_uncategorized:
-        query = query.filter(lambda t: t.category_id == TBD_CATEGORY_ID)
-
-    account = accounts.get_by_id(app, account_id)
-    if account is not accounts.ALL_ACCOUNTS:
-        query = query.filter(lambda t: t.account == account.id)
-
-    total = query.count()
-    query = query.order_by(Transaction.date.desc()).limit(per_page, offset=(page - 1) * per_page)
+    query = query.order_by(Transaction.date.desc())
+    query = paginator(query)
     transactions = query[:]
 
     return {
-        'total_pages': int(math.ceil(1.0 * total / per_page)),
-        'current_page': page,
+        'total_pages': paginator.total_pages,
+        'current_page': paginator.page,
         'transactions': [txn.to_dict() for txn in transactions]
     }
 
--- server/tests/integration/test_transactions.py
+++ server/tests/integration/test_transactions.py
@@ -1,5 +1,4 @@
 import datetime
-import pytest
 import requests
 from .utils import reset_database
 from .fixtures.transactions import create, fake
@@ -16,7 +15,7 @@ def test_transactions_get___empty():
     assert response.json() == {'transactions': [], 'total_pages': 0, 'current_page': 1}
 
 
-def test_transactions_get___tangerine_account(tangerine_credit_card_account):
+def test_transactions_get___tangerine_account(tangerine_credit_card_account):  # noqa
     for _ in xrange(5):
         create(account=tangerine_credit_card_account['id'])
     response = requests.get('http://localhost:7000/api/accounts/4')
@@ -27,7 +26,7 @@ def test_transactions_get___tangerine_account(tangerine_credit_card_account):
     assert response_json['total_pages'] == 1
 
 
-def test_transactions_get___pagination(tangerine_credit_card_account):
+def test_transactions_get___pagination(tangerine_credit_card_account):  # noqa
     for _ in xrange(5):
         create(account=tangerine_credit_card_account['id'])
     response = requests.get('http://localhost:7000/api/accounts/4?perPage=4&page=1')
@@ -37,7 +36,7 @@ def test_transactions_get___pagination(tangerine_credit_card_account):
     assert response_json['total_pages'] == 2
 
 
-def test_transactions_get___pagination___edge_case(tangerine_credit_card_account):
+def test_transactions_get___pagination___edge_case(tangerine_credit_card_account):  # noqa
     for _ in xrange(6):
         create(account=tangerine_credit_card_account['id'])
     response = requests.get('http://localhost:7000/api/accounts/4?perPage=2&page=3')
@@ -47,7 +46,7 @@ def test_transactions_get___pagination___edge_case(tangerine_credit_card_account
     assert response_json['total_pages'] == 3
 
 
-def test_transactions_get___transactions_from_multiple_accounts_are_available_under_all(
+def test_transactions_get___transactions_from_multiple_accounts_are_available_under_all(  # noqa
         tangerine_credit_card_account, rogers_bank_credit_card_account):
     for _ in xrange(5):
         create(account=tangerine_credit_card_account['id'])
@@ -60,7 +59,7 @@ def test_transactions_get___transactions_from_multiple_accounts_are_available_un
     assert response_json['current_page'] == 1
 
 
-def test_transactions_get___transactions_in_date_range(tangerine_credit_card_account):
+def test_transactions_get___transactions_in_date_range(tangerine_credit_card_account):  # noqa
     # before startDate
     for _ in xrange(2):
         create(account=tangerine_credit_card_account['id'],
@@ -85,3 +84,31 @@ def test_transactions_get___transactions_in_date_range(tangerine_credit_card_acc
     assert response.status_code == 200
     response_json = response.json()
     assert 4 == len(response_json['transactions'])
+
+
+# def test_transactions_get___order_by_date_asc(tangerine_credit_card_account):  # noqa
+#     for _ in xrange(10):
+#         create(account=tangerine_credit_card_account['id'],
+#                date=fake.date_time_between_dates(
+#                    datetime_start=datetime.datetime(2017, 1, 1),
+#                    datetime_end=datetime.datetime(2017, 2, 28)).date())
+
+#     response = requests.get('http://localhost:7000/api/accounts/_all?orderBy=date_asc')
+#     assert response.status_code == 200
+#     response_json = response.json()
+#     dates = [r['date'] for r in response_json['transactions']]
+#     assert dates == sorted(dates)
+
+
+# def test_transactions_get___order_by_date_desc(tangerine_credit_card_account):  # noqa
+#     for _ in xrange(10):
+#         create(account=tangerine_credit_card_account['id'],
+#                date=fake.date_time_between_dates(
+#                    datetime_start=datetime.datetime(2017, 1, 1),
+#                    datetime_end=datetime.datetime(2017, 2, 28)).date())
+
+#     response = requests.get('http://localhost:7000/api/accounts/_all?orderBy=date_desc')
+#     assert response.status_code == 200
+#     response_json = response.json()
+#     dates = [r['date'] for r in response_json['transactions']]
+#     assert dates == reversed(sorted(dates))
