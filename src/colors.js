2b3f28497badeb9b39309378ca62ec743d8a06ec
Kevin Jing Qiu <kevin@idempotent.ca>
2017-05-12 00:02:58 -0400

    Extract color constants

---
+++ src/colors.js
@@ -0,0 +1,2 @@
+const COLORS = ['#EFBDEB', '#B68CB8', '#6461A0', '#314CB6', '#0A81D1', '#9F7E69', '#D2BBA0', '#F2EFC7', '#F7FFE0', '#FFEEE2']
+export default COLORS
--- src/components/accounts/CategorySummaryPanel.js
+++ src/components/accounts/CategorySummaryPanel.js
@@ -5,8 +5,7 @@ import accounting from 'accounting'
 import Spinner from '../Spinner'
 import './CategorySummaryPanel.css'
 import { Cell, Pie, PieChart, Tooltip } from 'recharts'
-
-const COLORS = ['#EFBDEB', '#B68CB8', '#6461A0', '#314CB6', '#0A81D1', '#9F7E69', '#D2BBA0', '#F2EFC7', '#F7FFE0', '#FFEEE2']
+import COLORS from '../../colors'
 
 const SummaryTable = ({data}) => {
     return (
